b7049d0631b431eb83b0ebd5b7eb0691349d91d6
Ian Zanger <izanger@purdue.edu>
2017-05-30 11:07:34 -0400

    Add search form for Github component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Switch, Route, NavLink} from 'react-router-dom'
 import logo from './logo.svg';
+import Github from './Github'
 import './App.css';
 
 class App extends Component {
@@ -22,7 +23,7 @@ class App extends Component {
           </ul>
         </div>
         <Switch>
-          <Route path='/github' render={() => <h1>GITHUB!</h1>}/>
+          <Route path='/github' component={Github} />
           <Route render={() => <p>To get started, click one of the links above</p>} />
         </Switch>
       </div>
